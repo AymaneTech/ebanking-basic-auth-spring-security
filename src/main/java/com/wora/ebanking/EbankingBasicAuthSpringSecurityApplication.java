e968303a040f897b08a3232554a35e065b00d209
geek <elmainiaymane03@gmail.com>
2024-12-02 10:19:46 +0100

    setup the project

---
+++ src/main/java/com/wora/ebanking/EbankingBasicAuthSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.wora.ebanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbankingBasicAuthSpringSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbankingBasicAuthSpringSecurityApplication.class, args);
+    }
+
+}
